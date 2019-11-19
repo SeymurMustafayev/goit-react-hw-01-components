6e025f5b9552767ca1710361f97033b1f976a59a
Seymur <mustafav.seymur@gmail.com>
2019-11-19 17:47:07 +0200

    Updates

---
+++ precache-manifest.fc2f6a6636389b633f6adab250434035.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9167f53b156835e03f4b7707b703464b",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "0cd5368cca4a93e3e15c",
+    "url": "/goit-react-hw-01-components/static/css/main.9fbaf8c2.chunk.css"
+  },
+  {
+    "revision": "8133a7c02dd3de87deb4",
+    "url": "/goit-react-hw-01-components/static/js/2.453bfba5.chunk.js"
+  },
+  {
+    "revision": "0cd5368cca4a93e3e15c",
+    "url": "/goit-react-hw-01-components/static/js/main.2f1d77bf.chunk.js"
+  },
+  {
+    "revision": "76c0918716ad9d7cd3e5",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.17fd7762.js"
+  }
+]);
