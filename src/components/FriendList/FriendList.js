3295602bbd46c437719931c8315216a68e7eccb4
Seymur <mustafav.seymur@gmail.com>
2019-11-21 16:38:29 +0200

    qwqw

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FriendListItem from './FriendListItem';
+import s from './FriendList.module.css';
 
 const FriendList = ({ friends }) => (
-  <ul className="friendList">
+  <ul className={s.friendList}>
     {friends.map(el => (
       <FriendListItem
         key={el.id}
